97bad83a50325dd405ec986754f1cc081b24c4a8
Useles5 <126971764+Useles5@users.noreply.github.com>
2026-06-19 20:43:39 +0530

    feat: display accepted Codeforces submissions

--- main.go
+++ main.go
@@ -1,23 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 )
+
+type Verdict string
+
+const (
+	VerdictOK                Verdict = "OK"
+	VerdictTimeLimitExceeded Verdict = "TIME_LIMIT_EXCEEDED"
+	VerdictWrongAnswer       Verdict = "WRONG_ANSWER"
+	// for now just these
+)
+
+type CFResponse struct {
+	Status string         `json:"status"`
+	Result []CFSubmission `json:"result"`
+}
+
+type CFSubmission struct {
+	ID                  int     `json:"id"`
+	CreationTimeSeconds int     `json:"creationTimeSeconds"`
+	Verdict             Verdict `json:"verdict,omitempty"`
+	Problem             Problem `json:"problem"`
+}
+
+type Problem struct {
+	ContestID int    `json:"contestId"`
+	Index     string `json:"index"`
+	Name      string `json:"name"`
+}
 
 func main() {
 
-	userName := os.Args[1]
-	url := fmt.Sprintf("https://codeforces.com/api/user.status?handle=%s&from=1&count=10", userName)
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go userName")
 	}
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(body))
 
+	safeUserName := url.QueryEscape(os.Args[1])
+	apiURL := fmt.Sprintf("https://codeforces.com/api/user.status?handle=%s&from=1&count=100", safeUserName)
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	resp, err := client.Get(apiURL)
+	if err != nil {
+		log.Fatalf("Failed to execute request: %v", err)
+	}
+	// Passing Body as an argument takes a snapshot of the variable right now.
+	// This prevents dangerous "changing variable" bugs if used inside a loop.
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("Failed to close response body: %v", err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("API returned non-200 status: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	var cfData CFResponse
+	err = json.NewDecoder(resp.Body).Decode(&cfData)
+	if err != nil {
+		log.Fatalf("Failed to parse json: %v", err)
+	}
+
+	foundSolved := false
+
+	for _, sub := range cfData.Result {
+		if sub.Verdict == VerdictOK {
+			foundSolved = true
+
+			fmt.Printf("%d%s - %s\n",
+				sub.Problem.ContestID,
+				sub.Problem.Index,
+				sub.Problem.Name)
+		}
+	}
+
+	if !foundSolved {
+		fmt.Println("No Successful submissions found in given range")
+	}
 }
