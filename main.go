877a3dfeef909dc13b98c7a4d52497f579344072
Useles5 <126971764+Useles5@users.noreply.github.com>
2026-06-20 22:58:18 +0530

    feat: support last N solved problems and refactor core logic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -38,59 +39,82 @@ type CFProblem struct {
 	Name      string `json:"name"`
 }
 
-func main() {
+type Options struct {
+	Handle string
+	Last   int
+}
 
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: go run main.go userName")
+func parseOptions() *Options {
+	lastFlag := flag.Int("last", 0, "Fetches N recent successful submissions")
+
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		log.Fatal("Usage: slvd [--last N] <codeforces-handle>")
 	}
 
-	safeUserName := url.QueryEscape(os.Args[1])
-	apiURL := fmt.Sprintf("https://codeforces.com/api/user.status?handle=%s&from=1&count=100", safeUserName)
+	return &Options{Handle: args[0], Last: *lastFlag}
+}
 
+func fetchCodeforcesSubmissions(opts *Options) ([]CFSubmission, error) {
+	safeUserName := url.QueryEscape(opts.Handle)
+	apiURL := fmt.Sprintf("https://codeforces.com/api/user.status?handle=%s&from=1&count=100", safeUserName)
 	client := &http.Client{
 		Timeout: time.Second * 10,
 	}
 
 	resp, err := client.Get(apiURL)
 	if err != nil {
-		log.Fatalf("Failed to execute request: %v", err)
+		return nil, fmt.Errorf("failed to execute request: %w", err) // %w to wrap a error
 	}
 	// Passing Body as an argument takes a snapshot of the variable right now.
 	// This prevents dangerous "changing variable" bugs if used inside a loop.
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Printf("Failed to close response body: %v", err)
+			log.Printf("warning: failed to cleanly close: %v", err)
 		}
 	}(resp.Body)
 
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("API returned non-200 status: %d %s", resp.StatusCode, resp.Status)
+		return nil, fmt.Errorf("API returned non-200 status: %d %s", resp.StatusCode, resp.Status)
 	}
 
 	var cfData CFResponse
 	err = json.NewDecoder(resp.Body).Decode(&cfData)
 	if err != nil {
-		log.Fatalf("Failed to parse json: %v", err)
+		return nil, fmt.Errorf("failed to parse json: %w", err)
 	}
 
+	return cfData.Result, nil
+}
+
+func getSolvedProblems(submission []CFSubmission, lastN int) ([]string, int) {
 	seen := make(map[string]struct{})
+	var solved []string
 
 	now := time.Now()
 	midnightUnix := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).Unix()
 
 	processed := 0
 
-	for _, sub := range cfData.Result {
+	for _, sub := range submission {
 
-		// API sends newest first.
-		// Once we hit yesterday's problem, we can safely break.
-		if sub.CreationTimeSeconds < midnightUnix {
-			break
+		if lastN > 0 {
+			if len(seen) == lastN {
+				break
+			}
+		} else {
+			// API sends newest first.
+			// Once we hit yesterday's problem, we can safely break.
+			if sub.CreationTimeSeconds < midnightUnix {
+				break
+			}
 		}
 
 		processed++
-		
+
 		if sub.Verdict != VerdictOK {
 			continue
 		}
@@ -103,13 +127,34 @@ func main() {
 
 		seen[problemKey] = struct{}{}
 
-		fmt.Printf("%s - %s\n", problemKey, sub.Problem.Name)
+		formattedString := fmt.Sprintf("%s - %s", problemKey, sub.Problem.Name)
+		solved = append(solved, formattedString)
 
 	}
 
-	if len(seen) == 0 {
+	return solved, processed
+}
+func main() {
+	opts := parseOptions()
+
+	subs, err := fetchCodeforcesSubmissions(opts)
+	if err != nil {
+		log.Fatalf("Failed to fetch Codeforces submissions: %v", err)
+	}
+
+	solvedProblems, processed := getSolvedProblems(subs, opts.Last)
+	for _, solvedProblem := range solvedProblems {
+		fmt.Println(solvedProblem)
+	}
+
+	if len(solvedProblems) == 0 {
 		fmt.Fprintf(os.Stderr, "No problems found\n")
 	}
 
-	fmt.Fprintf(os.Stderr, "fetched=%d processed_today=%d unique_solved=%d\n", len(cfData.Result), processed, len(seen))
+	if opts.Last > 0 {
+		fmt.Fprintf(os.Stderr, "[Mode: Last %d] fetched=%d processed=%d unique_solved=%d\n", opts.Last, len(subs), processed, len(solvedProblems))
+	} else {
+		fmt.Fprintf(os.Stderr, "[Mode: Today]  fetched=%d processed=%d unique_solved=%d\n", len(subs), processed, len(solvedProblems))
+	}
+
 }
